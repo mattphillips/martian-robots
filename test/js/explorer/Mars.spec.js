b6fbc0af7d6d8fa509a182302204ff73b9c4735c
Matt Phillips <mattphillips1016@gmail.com>
2016-04-15 16:36:23 +0100

    added logic to check if a robot is lost

--- test/js/explorer/Mars.spec.js
+++ test/js/explorer/Mars.spec.js
@@ -26,4 +26,22 @@ describe("should move robot for a given list of instructions", function () {
         expect(finalPosition).toEqual(new Position(2, 2, Orientation.WEST));
     });
 
+    it("should move robot to fall off the edge of mars and set the position to lost", function () {
+        var robot = new Robot(
+            new Position(0, 0, Orientation.NORTH),
+            [
+                Instruction.FORWARD,
+                Instruction.FORWARD,
+                Instruction.FORWARD,
+                Instruction.FORWARD,
+                Instruction.FORWARD
+            ]
+        );
+
+        var mars = new Mars(5, 3);
+        var finalPosition = mars.move(robot);
+
+        expect(finalPosition).toEqual(new Position(0, 3, Orientation.NORTH, true));
+        expect(finalPosition.isLost()).toBeTruthy();
+    });
 });
